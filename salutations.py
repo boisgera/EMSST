1cf07d355dc0c5dcd4f4f9fd76b84e1f1f4db774
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2023-06-16 14:14:43 +0200

    👷 Add CI for doctests

---
+++ salutations.py
@@ -0,0 +1,11 @@
+def message_salutation(nom):
+    """
+    Retourne un message de salutation
+
+    Usage:
+
+        >>> import salutations
+        >>> salutations.message_salutation("Sébastien")
+        'Hello Sébastien !'
+    """
+    return f"Hello {nom} !"
